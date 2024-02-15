3de15749df573a9627bbb2143b5baaa64c39c094
ViFeduk <viktor2005fedchenkov@mail.ru>
2024-02-15 18:13:14 +0300

    Добавьте файлы проекта.

---
+++ modules 4.1/Program.cs
@@ -0,0 +1,35 @@
+﻿namespace modules_4._1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Напишите свой любимый цвет на английском буквы");
+
+            var color = Console.ReadLine().ToLower();
+
+            if (color == "red")
+            {
+                Console.BackgroundColor = ConsoleColor.Red;
+                Console.ForegroundColor = ConsoleColor.Black;
+
+                Console.WriteLine("Your color is red!");
+            }
+
+            else if (color == "green")
+            {
+                Console.BackgroundColor = ConsoleColor.Green;
+                Console.ForegroundColor = ConsoleColor.Black;
+
+                Console.WriteLine("Your color is green!");
+            }
+            else
+            {
+                Console.BackgroundColor = ConsoleColor.Cyan;
+                Console.ForegroundColor = ConsoleColor.Black;
+
+                Console.WriteLine("Your color is cyan!");
+            }
+        }
+    }
+}
